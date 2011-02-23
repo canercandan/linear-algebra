84c2f01e30d5b7cd558e5e920a0647e479a88de2
Caner Candan <caner@candan.fr>
2011-02-23 13:47:42 +0100

    + created the new class of Gemv inherits from MultiplyMatVecOp

---
+++ src/linear_algebra/detail/device/cublas/Gemv.h
@@ -0,0 +1,62 @@
+// -*- mode: c++; c-indent-level: 4; c++-member-init-indent: 8; comment-column: 35; -*-
+
+/* This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Authors:
+ * Caner Candan <caner@candan.fr>, http://caner.candan.fr
+ */
+
+#ifndef _linear_algebra_detail_device_cublas_Gemv_h
+#define _linear_algebra_detail_device_cublas_Gemv_h
+
+#include <cublas.h>
+
+#include "MultiplyMatVecOp.h"
+
+namespace linear_algebra
+{
+    namespace detail
+    {
+	namespace device
+	{
+	    namespace cublas
+	    {
+		template < typename Atom >
+		class Gemv : public MultiplyMatVecOp< Atom >
+		{
+		public:
+		    void operator()( const Matrix< Atom >&, const Vector< Atom >&, Vector< Atom >& );
+		};
+
+		template <>
+		void Gemv< float >::operator()( const Matrix< float >& A, const Vector< float >& x, Vector< float >& y )
+		{
+		    int n = A.rows();
+		    int m = A.cols();
+		    cublasSgemv('n', m, n, 1, A, m, x, 1, 0, y, 1);
+		}
+
+		template <>
+		void Gemv< double >::operator()( const Matrix< double >& A, const Vector< double >& x, Vector< double >& y )
+		{
+		    int n = A.rows();
+		    int m = A.cols();
+		    cublasDgemv('n', m, n, 1, A, m, x, 1, 0, y, 1);
+		}
+	    }
+	}
+    }
+}
+
+#endif // !_linear_algebra_detail_device_cublas_Gemv_h
