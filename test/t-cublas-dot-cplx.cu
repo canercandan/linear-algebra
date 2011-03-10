cd7368099eba0c91005eb30a036966b4bc1ad864
Caner Candan <caner@candan.fr>
2011-03-10 07:42:28 +0100

    + t-cublas-dot-cplx.cu

---
+++ test/t-cublas-dot-cplx.cu
@@ -0,0 +1,41 @@
+// -*- mode: c++; c-indent-level: 4; c++-member-init-indent: 8; comment-column: 35; -*-
+
+/* This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Authors:
+ * Caner Candan <caner@candan.fr>, http://caner.candan.fr
+ */
+
+#include <linear_algebra/detail/device/cublas/cublas>
+#include <linear_algebra/detail/device/cuda/cuda>
+
+using namespace linear_algebra::detail::device::cublas;
+using namespace linear_algebra::detail::device::cuda;
+
+typedef SingleComplex T;
+//typedef double Atom;
+
+int main(void)
+{
+    const int N = 4;
+
+    Vector<T> x(N, 1);
+    Vector<T> y(N, 2);
+
+    //Dot< Vector< Atom > > dot;
+
+    core_library::logger << "dot result: " << dot( x, y ) << std::endl;
+
+    return 0;
+}
